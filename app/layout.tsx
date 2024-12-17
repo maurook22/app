7191c71b10d85fbbf477aaf14f96f418ac0fc2c0
Mauro Nahuel Abraham <maauro.db@gmail.com>
2024-12-17 19:30:06 -0300

    commit inicial

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import { lusitana } from './ui/fonts';
+import './ui/global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={` ${lusitana.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Montserrat, Lusitana, Inter } from 'next/font/google'
+
+export const montserrat = Montserrat({ subsets: ['latin']})
+
+export const lusitana = Lusitana({ weight: '400'})
+
+export const inter = Inter({ weight: '400'})
